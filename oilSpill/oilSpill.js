c4df6f54831c903e7dd76319be9fcf93e4f6967b
franceneee <57038559+franceneee@users.noreply.github.com>
2022-06-11 12:37:36 +0800

    Draw kelp hair
    buggy, the hair flashes

--- oilSpill/oilSpill.js
+++ oilSpill/oilSpill.js
@@ -22,6 +22,80 @@ window.onload = function () {
         mouse.x = e.clientX - canvas.offsetLeft
         mouse.y = e.clientY - canvas.offsetTop
     });
+    // Make image follow mouse
+    const playerLeft = new Image();
+    playerLeft.src = "../Assets/images/seaweedhair-l.svg";
+    playerLeft.style.objectFit = "contain";
+    const playerRight = new Image();
+    playerRight.src = "../Assets/images/seaweedhair-r.svg";
+    playerRight.style.objectFit = "contain";
+
+    class Player {
+        constructor(){
+            this.x = canvas.width;
+            this.y = canvas.height/2;
+            this.radius = 50;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.frame = 0;
+            this.spriteWidth = 135;
+            this.spriteHeight = 1000;
+        }
+        update(){
+            const dx = this.x - mouse.x;
+            const dy = this.y - mouse.y;
+            if (mouse.x != this.x){
+                this.x -= dx/20;
+                this.moving = true;
+            }
+            if (mouse.y != this.y){
+                this.y -= dy/20;
+                this.moving = true;
+            }
+            if (this.x < 0) this.x = 0;
+            if (this.x > canvas.width) this.x = canvas.width;
+            if (this.y < 50) this.y = 50;
+            if (this.y > canvas.height) this.y = canvas.height;
+        }
+        draw(){
+            if (mouse.click){
+                ctx.lineWidth = 0.2;
+                ctx.beginPath();
+                ctx.moveTo(this.x, this.y);
+                ctx.lineTo(mouse.x, mouse.y);
+                ctx.stroke();
+            }
+            if (gameFrame % 10 == 0) {
+                this.frame++;
+                if (this.frame >= 12) this.frame = 0;
+                if ( this.frame == 3 ||  this.frame == 7 ||  this.frame == 11) {
+                    this.frameX = 0;
+                } else this.frameX++;
+                if (this.frame < 3){
+                    this.frameY = 0;
+                } else if (this.frame < 7){
+                    this.frameY = 1;
+                } else if (this.frame < 11){
+                    this.frameY = 2;
+                } else this.frameY = 0;
+            }
+          
+            ctx.fillStyle = 'black';
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.rotate(this.angle);
+            //ctx.beginPath();
+            //ctx.arc(0, 0, this.radius, 0, Math.PI * 360);
+            //ctx.fill();
+            if (this.x >= mouse.x){
+                ctx.drawImage(playerLeft, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0 - 60, 0 - 45, this.spriteWidth * 0.8, this.spriteHeight * 0.8);
+            } else {
+                ctx.drawImage(playerRight, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0 - 60, 0 - 45, this.spriteWidth * 0.8, this.spriteHeight * 0.8);
+            }
+            ctx.restore();
+        }
+    }
+    const player = new Player();
 
     // Make oil droplets
     const oilArray = [];
@@ -36,7 +110,6 @@ window.onload = function () {
             this.radius = 65;
             this.speed = Math.random() * -5 + -1;
             this.distance;
-            this.sound = Math.random() <= 0.5 ? "sound1" : "sound2";
             this.counted = false;
             this.frameX = 0;
             this.spriteWidth = 130;
