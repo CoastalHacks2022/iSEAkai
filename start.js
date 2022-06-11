b82b6b40369982f37c47c54d34ff9ae7df1c697f
franceneee <57038559+franceneee@users.noreply.github.com>
2022-06-11 22:35:46 +0800

    Add and update image assets
    changed skin colour

---
+++ start.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    const nextButton = document.getElementById("next");
+    const blinkingRozxie = document.getElementById("rozxie");
+    const oilSpillNav = document.getElementById("oilSpill");
+    const screens = ['start', 'litter', 'coral']
+    
+    let i = 0;
+
+    nextButton.addEventListener("click", () => {
+        console.log(i)
+        if (i !== 0) document.getElementById(screens[i-1]).style.display = "none";
+        if (i == 1) blinkingRozxie.style.display = "none";
+        document.getElementById(screens[i]).style.display = "flex";
+        i++;
+        if (i == screens.length) {
+            nextButton.style.display = "none";
+            oilSpillNav.style.display = "block";
+        }      
+    })
+}
