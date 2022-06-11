5b65caebe6efebdc65b9faca71aba4775f497158
franceneee <57038559+franceneee@users.noreply.github.com>
2022-06-12 00:54:45 +0800

    Add break curse scene

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const nextButton = document.getElementById("next");
     const blinkingRozxie = document.getElementById("rozxie");
     const oilSpillNav = document.getElementById("oilSpill");
-    const screens = ['start', 'litter', 'coral', 'marine', 'kelpie', 'curse']
+    const screens = ['start', 'litter', 'coral', 'marine', 'kelpie', 'curse', 'breakcurse']
     const scream = document.getElementById("scream");
     
     let i = 0;
