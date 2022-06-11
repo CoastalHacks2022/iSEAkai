1b4506553d4ef6b4d4c446ad299109288e1817d0
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-06-11 23:49:07 +0800

    badFishing minigame complete (sorta)

--- badFishing/badFishingV2.js
+++ badFishing/badFishingV2.js
@@ -1,5 +1,15 @@
 // CONSTANTS
 
+// VARIABLES
+
+var animalFree = {
+  dolphin: false,
+  shark: false,
+  whaleshark: false,
+  turtle: false,
+  stingray: false,
+};
+
 // MATTER JS
 
 // define constants for matter.js
@@ -41,9 +51,9 @@ window.onload = function () {
     options: {
       width: 800,
       height: 600,
-      showAngleIndicator: true,
-      showCollisions: true,
-      showVelocity: true,
+      // showAngleIndicator: true,
+      // showCollisions: true,
+      // showVelocity: true,
       wireframes: false,
       background: "#03a6e1",
     },
@@ -58,11 +68,7 @@ window.onload = function () {
   var defaultCategory = 0x0001,
     tentacleCategory = 0x0002,
     netCategory = 0x0004,
-    dolphinCategory = 0x0008;
-    turtleCategory = 0x0010;
-    sharkCategory = 0x0020;
-    stingrayCategory = 0x0040;
-    whalesharkCategory = 0x0040;
+    animalCategory = 0x0008;
 
   // add mouse control
   var mouse = Mouse.create(render.canvas),
@@ -81,7 +87,90 @@ window.onload = function () {
   // keep the mouse in sync with rendering
   render.mouse = mouse;
 
-  
+  /////////////// BORDERS ///////////////
+
+  // define a static ground (the *2 for width is cause canvas width is half the actual thing apparently)
+  var ground = Bodies.rectangle(
+    render.canvas.width / 2 - 50,
+    render.canvas.height,
+    render.canvas.width,
+    50,
+    {
+      collisionFilter: {
+        category: animalCategory,
+        mask: animalCategory,
+      },
+      isStatic: true,
+      render: {
+        fillStyle: "#36c247",
+        strokeStyle: "green",
+        lineWidth: 0,
+        visible: false,
+      },
+    }
+  );
+
+  var ceiling = Bodies.rectangle(
+    render.canvas.width / 2 - 50,
+    0,
+    render.canvas.width,
+    50,
+    {
+      collisionFilter: {
+        category: animalCategory,
+        mask: animalCategory,
+      },
+      isStatic: true,
+      render: {
+        fillStyle: "#36c247",
+        strokeStyle: "green",
+        lineWidth: 0,
+        visible: false,
+      },
+    }
+  );
+
+  var leftWall = Bodies.rectangle(
+    -50,
+    render.canvas.height / 2,
+    50,
+    render.canvas.height,
+    {
+      collisionFilter: {
+        category: animalCategory,
+        mask: animalCategory,
+      },
+      isStatic: true,
+      render: {
+        fillStyle: "#36c247",
+        strokeStyle: "green",
+        lineWidth: 0,
+        visible: false,
+      },
+    }
+  );
+
+  var rightTrigger = Bodies.rectangle(
+    render.canvas.width - 50,
+    render.canvas.height / 2,
+    50,
+    render.canvas.height,
+    {
+      isSensor: true,
+      collisionFilter: {
+        category: animalCategory,
+        mask: animalCategory,
+      },
+      isStatic: true,
+      render: {
+        fillStyle: "#0330fc",
+        strokeStyle: "green",
+        lineWidth: 0,
+        visible: false,
+      },
+    }
+  );
+
   /////////////// TENTACLES ///////////////
 
   group = Body.nextGroup(true);
@@ -103,6 +192,7 @@ window.onload = function () {
       return Bodies.rectangle(x - 20, y, 50, 20, {
         collisionFilter: {
           category: tentacleCategory,
+          mask: tentacleCategory,
           group: group,
         },
         render: {
@@ -124,7 +214,8 @@ window.onload = function () {
     20,
     {
       collisionFilter: {
-        category: tentacleCategory,
+        category: tentacleCategory | animalCategory,
+        mask: tentacleCategory | animalCategory,
         group: group,
       },
       render: {
@@ -148,6 +239,7 @@ window.onload = function () {
     {
       collisionFilter: {
         category: tentacleCategory,
+        mask: tentacleCategory,
         group: group,
       },
       render: {
@@ -179,17 +271,17 @@ window.onload = function () {
     })
   );
 
-
   /////////////// NET ///////////////
 
   var net = Bodies.rectangle(
-    render.canvas.width / 2 - 200,
-    render.canvas.height / 2 - 30,
+    render.canvas.width / 2 - 150,
+    render.canvas.height / 2,
     render.canvas.width,
     20,
     {
       collisionFilter: {
         category: netCategory,
+        mask: netCategory,
         group: group,
       },
       isStatic: true,
@@ -200,61 +292,142 @@ window.onload = function () {
         sprite: {
           texture: "../assets/images/netTexture.png",
           xScale: 1.6,
-          yScale: 1.8,
+          yScale: 2,
         },
       },
     }
   );
 
+  /////////////// BY CATCH ///////////////
 
-  /////////////// FISH ///////////////
-
-  var dolphin = Bodies.rectangle(
-    render.canvas.width / 2 - 200,
-    render.canvas.height / 2 - 30,
+  var turtle = Bodies.rectangle(
+    render.canvas.width / 2,
+    render.canvas.height / 2,
     50,
     50,
     {
       collisionFilter: {
-        category: dolphinCategory,
-        group: group,
+        category: animalCategory | defaultCategory,
+        mask: animalCategory,
       },
-      isStatic: true,
       render: {
         fillStyle: "#36c247",
         strokeStyle: "green",
         lineWidth: 0,
         sprite: {
-          texture: "../assets/images/dolphin.png",
-          xScale: 0.5,
-          yScale: 0.5,
+          texture: "../assets/images/turtle.png",
+          xScale: 0.4,
+          yScale: 0.4,
         },
       },
     }
   );
 
+  var stingray = Bodies.rectangle(200, 200, 50, 50, {
+    collisionFilter: {
+      category: animalCategory | defaultCategory,
+      mask: animalCategory,
+    },
+    render: {
+      fillStyle: "#36c247",
+      strokeStyle: "green",
+      lineWidth: 0,
+      sprite: {
+        texture: "../assets/images/stingRay.png",
+        xScale: 0.4,
+        yScale: 0.4,
+      },
+    },
+  });
+
+  var shark = Bodies.rectangle(100, 150, 50, 50, {
+    collisionFilter: {
+      category: animalCategory | defaultCategory,
+      mask: animalCategory,
+    },
+    render: {
+      fillStyle: "#36c247",
+      strokeStyle: "green",
+      lineWidth: 0,
+      sprite: {
+        texture: "../assets/images/shark.png",
+        xScale: 0.4,
+        yScale: 0.4,
+      },
+    },
+  });
+
+  var dolphin = Bodies.rectangle(200, 500, 50, 50, {
+    collisionFilter: {
+      category: animalCategory | defaultCategory,
+      mask: animalCategory,
+    },
+    render: {
+      fillStyle: "#36c247",
+      strokeStyle: "green",
+      lineWidth: 0,
+      sprite: {
+        texture: "../assets/images/dolphin.png",
+        xScale: 0.4,
+        yScale: 0.4,
+      },
+    },
+  });
+
+  var whaleshark = Bodies.rectangle(500, 150, 50, 50, {
+    collisionFilter: {
+      category: animalCategory | defaultCategory,
+      mask: animalCategory,
+    },
+    render: {
+      fillStyle: "#36c247",
+      strokeStyle: "green",
+      lineWidth: 0,
+      sprite: {
+        texture: "../assets/images/whaleshark.png",
+        xScale: 0.4,
+        yScale: 0.4,
+      },
+    },
+  });
+
   /////////////// EVENTS ///////////////
 
   Events.on(engine, "collisionStart", function (event) {
-
     var pairs = event.pairs;
 
+    for (var i = 0, j = pairs.length; i != j; ++i) {
+      var pair = pairs[i];
+      if (pair.bodyA === rightTrigger || pair.bodyB === rightTrigger) {
+        if (pair.bodyA === dolphin || pair.bodyB === dolphin) {
+          animalFree.dolphin = true;
+        } else if (pair.bodyA === turtle || pair.bodyB === turtle) {
+          animalFree.turtle = true;
+        } else if (pair.bodyA === stingray || pair.bodyB === stingray) {
+          animalFree.stingray = true;
+        } else if (pair.bodyA === shark || pair.bodyB === shark) {
+          animalFree.shark = true;
+        } else if (pair.bodyA === whaleshark || pair.bodyB === whaleshark) {
+          animalFree.whaleshark = true;
+        }
+      }
+    }
+
+    if (checkAllFree()) {
+      alert("You win!");
+    }
   });
 
-  Events.on(engine, "collisionEnd", function (event) {
-    var pairs = event.pairs;
+  // Events.on(engine, "collisionEnd", function (event) {
+  //   var pairs = event.pairs;
+  // });
 
-  });
-
-  Events.on(mouseConstraint, "mousedown", function (event) {
-
-  });
+  // Events.on(mouseConstraint, "mousedown", function (event) {});
 
   Events.on(engine, "afterUpdate", function () {
     if (!mouse.position.x) {
       return;
     }
-
 
     if (
       Math.abs(mouse.position.x - tentacleEnd.position.x) < 10 &&
@@ -264,8 +437,6 @@ window.onload = function () {
         x: 0,
         y: 0,
       });
-
-
     } else {
       Body.setVelocity(tentacleEnd, {
         x: (mouse.position.x - tentacleEnd.position.x) * 0.1,
@@ -276,16 +447,17 @@ window.onload = function () {
 
   // add all of the bodies to the world
   World.add(engine.world, [
-    // ground,
-    // ceiling,
-    // leftWall,
-    // rightWall,
+    ground,
+    ceiling,
+    leftWall,
+    rightTrigger,
     tentacleComposite,
     net,
-    // dolphin,
-    // circle,
-    // constraintTip,
-    // staticBody,
+    turtle,
+    stingray,
+    shark,
+    dolphin,
+    whaleshark,
   ]);
 
   // fit the render viewport to the scene
@@ -302,3 +474,13 @@ window.onload = function () {
 
   console.log("All loaded!");
 };
+
+function checkAllFree() {
+  return (
+    animalFree.dolphin &&
+    animalFree.turtle &&
+    animalFree.whaleshark &&
+    animalFree.shark &&
+    animalFree.stingray
+  );
+}
