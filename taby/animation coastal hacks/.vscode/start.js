b6d4ec203d0ecfb4b415cb38bbc5cf90c646ed13
tabydot <tabyspaams@gmail.com>
2022-06-12 18:30:04 +0800

    outroscene

---
+++ taby/animation coastal hacks/.vscode/start.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    const nextButton = document.getElementById("next");
+    const blinkingRozxie = document.getElementById("rozxie");
+    const oilSpillNav = document.getElementById("oilSpill");
+    const screens = ['start', 'litter', 'coral', 'marine', 'kelpie', 'curse', 'breakcurse']
+    const scream = document.getElementById("scream");
+    
+    let i = 0;
+
+    nextButton.addEventListener("click", () => {
+        console.log(i)
+        if (i !== 0) document.getElementById(screens[i-1]).style.display = "none";
+        if (i == 1) blinkingRozxie.style.display = "none";
+        document.getElementById(screens[i]).style.display = "flex";
+        if (screens[i] === "curse") scream.play();
+        i++;
+        if (i == screens.length) {
+            nextButton.style.display = "none";
+            oilSpillNav.style.display = "block";
+        } 
+    })
+}
