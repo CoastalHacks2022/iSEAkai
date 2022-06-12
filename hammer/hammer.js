3b9f90cf4fa89b5f5f4fd794d3d43f6223915788
e0425705 <puahxiuwen@gmail.com>
2022-06-12 16:17:24 +0800

    add sounds + code

--- hammer/hammer.js
+++ hammer/hammer.js
@@ -1,52 +1,72 @@
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
+const squares = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
 
 // selecting for an element with id time-left
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+const timeLeft = document.querySelector('#time-left');
+const score = document.querySelector('#score');
 
-let result = 0
-let hitPosition
-let currentTime = 30
-let timerId = null
+let result = 0;
+let hitPosition;
+let currentTime = 30;
+let timerId = null;
 
 let countDownTimerId = setInterval(countDown, 1000)
 
 function randomSquare() {
   // remove mole
   squares.forEach(square => {
-    square.classList.remove('mole')
+    square.classList.remove('mole');
   })
 
   // add mole to random position from 1 - 32
-  let randomPosition = squares[Math.floor(Math.random() * 32)]
-  randomPosition.classList.add('mole')
-  hitPosition = randomPosition.id
+  let randomPosition = squares[Math.floor(Math.random() * 32)];
+  randomPosition.classList.add('mole');
+  hitPosition = randomPosition.id;
+}
+
+//create audio element for playing music and sfx
+function playSound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+
+  this.play = function () {
+    this.sound.play();
+  };
+  
+  this.stop = function () {
+    this.sound.pause();
+  };
 }
 
 squares.forEach(square => {
-  square.addEventListener('mouseover', () => {
+  square.addEventListener('click', () => {
     if (square.id == hitPosition) {
       result++;
-      score.textContent = result
-      hitPosition = null
+      score.textContent = result;
+      hitPosition = null;
+      smashedLoggers = new playSound("sounds/correct.mp3");
+      smashedLoggers.play();
     }
   })
 })
 
 function moveMole() {
-  timerId = setInterval(randomSquare, 800)
+  timerId = setInterval(randomSquare, 800);
 }
 
-moveMole()
+moveMole();
 
 function countDown() {
-  currentTime--
-  timeLeft.textContent = currentTime
+  currentTime--;
+  timeLeft.textContent = currentTime;
 
   if (currentTime == 0) {
-    clearInterval(countDownTimerId)
-    clearInterval(timerId)
-    alert('Game over! Your final score is ' + result)
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert('Game over! Your final score is ' + result);
   }
 }
